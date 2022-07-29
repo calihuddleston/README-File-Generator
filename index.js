ad6cd46e4ea99ee86174949279aafe68a0a10a6f
calihuddleston <calihuddleston@gmail.com>
2022-07-28 23:11:15 -0400

    'adding files with questions, some functions written, and file data'

---
+++ index.js
@@ -0,0 +1,74 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [{
+        type: 'input',
+        message: 'What is the title of your project?',
+        default: 'Project Title',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Enter a description for your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'If applicable, enter your installation instructions',
+        name: 'install'
+    },
+    {
+        type: 'input',
+        message: 'Provide instruction and examples for usage information',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'If applicable, enter your contribution guidelines',
+        name: 'guidelines'
+    },
+    {
+        type: 'input',
+        message: 'If applicable, enter your test instructions',
+        name: 'test'
+    },
+    {
+        type: 'checkbox',
+        message: 'What licensure would you like to apply?',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your GitHub username',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your GitHub profile',
+        name: 'profile'
+    },
+    {
+        type: 'input',
+        message: 'Please add your e-mail and instructions on how to reach you with additional questions',
+        name: 'email'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(data) {
+    const generateReadMe = generateMarkdown(data)
+
+    fs.writeFile('README.md', generateReadMe, (err) => {
+        err ? console.log(err) : console.log(`Successfully created ${fileName}!`)
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,55 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ${data.licenseBadge}
+
+  ## Table of Contents
+  -[description] (#description)
+  -[installation] (#install)
+  -[usage] (#usage)
+  -[guidelines] (#guidelines)
+  -[test instructions] (#test)
+  -[licenses] (#license)
+  -[GitHub Username] (#github)
+  -[GitHub Profile] (#profile)
+  -[Contact Information] (#email)
+
+  ##Description:
+  ${data.description}
+
+  ##Installation:
+  ${data.install}
+
+  ##Usage:
+  ${data.usage}
+
+  ##Guidelines:
+  ${data.guidelines}
+
+  ##Test Instructions:
+  ${data.test}
+
+  ##Profile:
+  ${data.profile}
+
+  ##Contact Information:
+  ${data.github}
+  ${data.profile}
+  ${data.email}
+`;
+}
+
+module.exports = generateMarkdown;
